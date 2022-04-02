06beffb9226aec1e4465f18b9dee0f80230d7db8
Willis Blackburn <wboyce@panix.com>
2022-04-02 17:08:24 -0400

    Change type of heap_ptr

--- test.h
+++ test.h
@@ -59,7 +59,7 @@ extern char buffer_length;
 extern line* line_ptr;
 #pragma zpsym ("line_ptr")
 extern line* program_ptr;
-extern line* heap_ptr;
+extern void* heap_ptr;
 
 // Prototypes for C wrapper functions
 
