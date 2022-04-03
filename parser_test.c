b0cfafce9f0f109f490a03d34faee6a24adb66a7
Willis Blackburn <wboyce@panix.com>
2022-04-03 10:46:08 -0400

    Backport from master: test.h

--- parser_test.c
+++ parser_test.c
@@ -1,10 +1,5 @@
 #include "test.h"
 
-static void set_buffer(const char* s) {
-    strcpy(buffer, s);
-    buffer_length = strlen(s);
-}
-
 static void test_char_to_digit(void) {
     int err;
 
@@ -30,39 +25,35 @@ static void test_char_to_digit(void) {
     ASSERT_NE(err, 0);
 }
 
-static void test_parse_number(void) {
+static void test_read_number(void) {
     int err;
 
     PRINT_TEST_NAME();
 
-    strcpy(buffer, "10 PRINT X");
-    buffer_length = 10;
+    set_buffer("10 PRINT X");
     err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 10);
 
     // The function should honor the current read index.
-    strcpy(buffer, "1020 PRINT X");
-    buffer_length = 12;
+    set_buffer("1020 PRINT X");
     err = read_number(2);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 20);
 
     // The function should skip inital whitespace.
-    strcpy(buffer, "  10000 PRINT X");
-    buffer_length = 15;
+    set_buffer("  10000 PRINT X");
     err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 10000);
 
     // The function should return carry set if an invalid number.
-    strcpy(buffer, "invalid");
-    buffer_length = 7;
+    set_buffer("invalid");
     err = read_number(0);
     ASSERT_NE(err, 0);
 
     // The function should not read past the end of the buffer.
-    strcpy(buffer, "10000");
+    set_buffer("10000");
     buffer_length = 3;
     err = read_number(0);
     ASSERT_EQ(err, 0);
@@ -98,7 +89,7 @@ void test_parse_keyword(void) {
 int main(void) {
     initialize_target();
     test_char_to_digit();
-    test_parse_number();
+    test_read_number();
     test_parse_keyword();
     return 0;
 }
