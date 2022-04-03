2efd6567caae67492b400b0f462b604bab245d4c
Willis Blackburn <wboyce@panix.com>
2022-04-03 10:56:25 -0400

    Backport from master: parser tests

--- parser_test.c
+++ parser_test.c
@@ -34,37 +34,35 @@ static void test_read_number(void) {
     err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 10);
+    ASSERT_EQ(r, 2);
 
-    // The function should honor the current read index.
+    // The function should honor the current read position.
     set_buffer("1020 PRINT X");
     err = read_number(2);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 20);
+    ASSERT_EQ(r, 4);
 
     // The function should skip inital whitespace.
     set_buffer("  10000 PRINT X");
     err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 10000);
+    ASSERT_EQ(r, 7);
 
     // The function should return carry set if an invalid number.
     set_buffer("invalid");
     err = read_number(0);
     ASSERT_NE(err, 0);
+    ASSERT_EQ(r, 0);
 
-    // The function should not read past the end of the buffer.
-    set_buffer("10000");
-    buffer_length = 3;
-    err = read_number(0);
-    ASSERT_EQ(err, 0);
-    ASSERT_EQ(reg_ax, 100);
-
-    buffer_length = 0;
+    set_buffer("");
     err = read_number(0);
     ASSERT_NE(err, 0);
+    ASSERT_EQ(r, 0);
 }
 
-void test_parse_keyword(void) {
+static void test_parse_keyword(void) {
     int err;
     const char* print = "PRIN\xD4";
 
@@ -80,10 +78,6 @@ void test_parse_keyword(void) {
     err = parse_keyword("PRIN\xD4", 2);
     ASSERT_NE(err, 0);
 
-    // The function should pay attention to buffer_length.
-    buffer_length = 3;
-    err = parse_keyword("PRIN\xD4", 0); // \xD4 = 'T' with high bit set
-    ASSERT_NE(err, 0);
 }
 
 int main(void) {
