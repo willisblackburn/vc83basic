b650c0537448671043540680155d171388e6fe1c
Willis Blackburn <wboyce@panix.com>
2022-04-02 20:04:34 -0400

    read_number

--- parser_test.c
+++ parser_test.c
@@ -37,39 +37,39 @@ static void test_parse_number(void) {
 
     strcpy(buffer, "10 PRINT X");
     buffer_length = 10;
-    err = parse_number(0);
+    err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 10);
 
     // The function should honor the current read index.
     strcpy(buffer, "1020 PRINT X");
     buffer_length = 12;
-    err = parse_number(2);
+    err = read_number(2);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 20);
 
     // The function should skip inital whitespace.
     strcpy(buffer, "  10000 PRINT X");
     buffer_length = 15;
-    err = parse_number(0);
+    err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 10000);
 
     // The function should return carry set if an invalid number.
     strcpy(buffer, "invalid");
     buffer_length = 7;
-    err = parse_number(0);
+    err = read_number(0);
     ASSERT_NE(err, 0);
 
     // The function should not read past the end of the buffer.
     strcpy(buffer, "10000");
     buffer_length = 3;
-    err = parse_number(0);
+    err = read_number(0);
     ASSERT_EQ(err, 0);
     ASSERT_EQ(reg_ax, 100);
 
     buffer_length = 0;
-    err = parse_number(0);
+    err = read_number(0);
     ASSERT_NE(err, 0);
 }
 
