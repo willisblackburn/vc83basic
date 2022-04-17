0ddd432d6f3ab6a6eb949b211b6560707e0b19bd
Willis Blackburn <wboyce@panix.com>
2022-04-17 15:31:31 -0400

    Backport debug changes

--- target_sim6502.s
+++ target_sim6502.s
@@ -119,6 +119,9 @@ save_a: .res 1
 save_x: .res 1
 save_y: .res 1
 save_sp: .res 1
+save_flags: .res 1
+
+flag_indicators: .res 8
 
 .macro  push8   value
         lda     value
@@ -142,7 +145,8 @@ save_sp: .res 1
 
 .code
 
-format: .byte "$%02X: A=%02X X=%02X Y=%02X SP=%02X", $0A, $00
+format: .byte "$%02X: A=%02X X=%02X Y=%02X SP=%02X %.8s", $0A, $00
+flag_names: .byte "NV-BDIZC"
 
 ; Prints the register values to stderr.
 ; Since this function calls the C library function fprintf, it saves all the C zero page registers and
@@ -151,19 +155,31 @@ format: .byte "$%02X: A=%02X X=%02X Y=%02X SP=%02X", $0A, $00
 ; doesn't generate interrupts, this will only be called by a BRK statement.
 
 debug_handler:
-        cld                     ; Clear decimal flag (just in case)
-        sta     save_a          ; Save 6502 registers
-        stx     save_x
-        sty     save_y
-        tsx                     ; Get stack pointer into X
-        stx     save_sp         ; Save it so we can print it
-        ldy     $102,x          ; PC low byte
-        sty     save_pc
-        ldy     $103,x          ; PC high byte
-        dey                     ; Subtract 256 from PC; we will index with Y = 255 to get PC-1
-        sty     save_pc+1
-        ldx     #0              ; Prepare to save cc65 registers
-        push16  sreg
+        cld                             ; Clear decimal flag (just in case)
+        sta     save_a                  ; Save 6502 registers
+        stx     save_x      
+        sty     save_y      
+        tsx                             ; Get stack pointer into X
+        stx     save_sp                 ; Save it so we can print it
+        ldy     $102,x                  ; PC low byte
+        sty     save_pc     
+        ldy     $103,x                  ; PC high byte
+        dey                             ; Subtract 256 from PC; we will index with Y = 255 to get PC-1
+        sty     save_pc+1       
+        lda     $101,x                  ; Flags
+        sta     save_flags
+        ldy     #0
+@next_flag:
+        lda     flag_names,y            ; Store name in indicator string if flag on
+        rol     save_flags
+        bcs     @on
+        lda     #'-'                    ; Store '-' indicator string if flag off
+@on:
+        sta     flag_indicators,y
+        iny
+        cpy     #8
+        bne     @next_flag
+        push16  sreg        
         push8   tmp1
         push8   tmp2
         push8   tmp3
@@ -172,26 +188,28 @@ debug_handler:
         push16  ptr2
         push16  ptr3
         push16  ptr4
-        lda     _stderr         ; fprintf(stderr, ...
+        lda     _stderr                 ; fprintf(stderr, ...
         ldx     _stderr+1
         jsr     pushax
-        lda     #<format        ; format, ...
+        lda     #<format                ; format, ...
         ldx     #>format
         jsr     pushax
         ldy     #$FF
-        lda     (save_pc),y     ; id, ...
+        lda     (save_pc),y             ; id, ...
         jsr     pusha0          
-        lda     save_a          ; A, ...
+        lda     save_a                  ; A, ...
         jsr     pusha0
-        lda     save_x          ; X, ...
+        lda     save_x                  ; X, ...
         jsr     pusha0
-        lda     save_y          ; Y, ...
+        lda     save_y                  ; Y, ...
         jsr     pusha0
-        lda     save_sp         ; SP)
+        lda     save_sp                 ; SP, ...
         jsr     pusha0
-        ldy     #14             ; 14 bytes on the C stack
+        lda     #<flag_indicators       ; flag_indicators)
+        ldx     #>flag_indicators
+        jsr     pushax           
+        ldy     #16                     ; 16 bytes on the C stack
         jsr     _fprintf
-        ldx     #0              ; Prepare to restore cc65 registers
         pull16  ptr4
         pull16  ptr3
         pull16  ptr2
@@ -201,7 +219,7 @@ debug_handler:
         pull8   tmp2
         pull8   tmp1
         pull16  sreg
-        lda     save_a          ; Restore 6502 registers
+        lda     save_a                  ; Restore 6502 registers
         ldx     save_x
         ldy     save_y
         rti
