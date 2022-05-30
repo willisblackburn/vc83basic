942d8a28f46d4f5c90335a1bc092ea4277df281d
Willis Blackburn <wboyce@panix.com>
2022-05-30 19:08:57 -0400

    Remove comment

--- util.s
+++ util.s
@@ -23,8 +23,6 @@ E: .res 1
 
 .code
 
-; TODO: maybe use AX, BC, DE for copy parameters
-
 ; Copies bytes from a source address to a destination address.
 ; The source and destination byte ranges must not overlap unless the destination address is lower than the
 ; source address.
