6a2ecf702e064fff5c8c9f477fec57f95b8ba99f
Willis Blackburn <wboyce@panix.com>
2022-10-02 11:15:40 -0400

    Move set_variable_value and set_variable_value_ptr

--- c_wrappers.s
+++ c_wrappers.s
@@ -279,10 +279,6 @@ _insert_or_update_line:
         jsr     insert_or_update_line
         jmp     return_carry
 
-_set_variable_value_ptr:
-.export _set_variable_value_ptr
-        jmp     set_variable_value_ptr
-
 _expand:
 .export _expand
         stax    BC                      ; Save size temporarily
@@ -312,6 +308,10 @@ _check_himem:
 .export _check_himem
         jsr     check_himem
         jmp     return_carry
+
+_set_variable_value_ptr:
+.export _set_variable_value_ptr
+        jmp     set_variable_value_ptr
 
 ; util.s
 
