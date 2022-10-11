f4681229d3febc0e0ce3f9aed7b3f1bd8eb039f6
Willis Blackburn <wboyce@panix.com>
2022-10-11 17:29:15 -0400

    Make decode_test parse number token

--- decode.s
+++ decode.s
@@ -2,14 +2,13 @@
 .include "basic.inc"
 
 ; Functions to decode values from the token stream.
-; Sometimes these functions will be called when one value has already been read and is in the A register;
-; this will be noted.
 ; We don't have to worry about errors since we're decoding what we previously encoded.
 ; For all functions, lp is the read position in line_ptr.
 
 ; Decodes a number and returns it in AX.
 
 decode_number:
+        inc     lp                      ; Advance past number marker token
         inc     lp                      ; Increment read position to high byte 
         ldy     lp                      ; Load position of high byte into Y
         inc     lp                      ; Increment read one position again
@@ -18,6 +17,13 @@ decode_number:
         dey                             ; Decrement Y
         lda     (line_ptr),y            ; Get the low byte of the number into A
         rts     
+
+decode_variable:
+        ldy     lp
+        inc     lp
+        lda     (line_ptr),y
+        and     #$7F                    ; Clear MSB
+        rts
 
 ; Decodes a single byte and returns it in A.
 ; The last instruction loads A, so this function will return with the Z and N flags set accordingly.
