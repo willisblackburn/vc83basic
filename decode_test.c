3b9dc2fb54dfa141e8710e496d2d18883b6efcc4
Willis Blackburn <wboyce@panix.com>
2022-11-20 12:44:14 -0500

    Eliminate extra parameters to encode/decode tests

--- decode_test.c
+++ decode_test.c
@@ -2,31 +2,47 @@
 
 static void test_decode_byte(void) {
     char byte_value;
-    const char line_data[] = { 0, 0, 1, 3 };
+    Line line = {
+        6,
+        10,
+        { 0x00, 0x01, 0x03 }
+    };
 
     PRINT_TEST_NAME();
 
-    byte_value = decode_byte(line_data, 0);
-    ASSERT_EQ(byte_value, 0);
+    line_ptr = &line;
+    lp = offsetof(Line, data);
 
-    byte_value = decode_byte(line_data, 2);
-    ASSERT_EQ(byte_value, 1);
+    byte_value = decode_byte();
+    ASSERT_EQ(byte_value, 0x00);
+
+    byte_value = decode_byte();
+    ASSERT_EQ(byte_value, 0x01);
+
+    byte_value = decode_byte();
+    ASSERT_EQ(byte_value, 0x03);
 }
 
 static void test_decode_number(void) {
     int value;
-    const char line_data_1[] = { TOKEN_NUM, 0, 0, TOKEN_NUM, 0, 1 };
-    const char line_data_2[] = { TOKEN_NUM, 1, 3 };
+    Line line = {
+        12,
+        10,
+        {  TOKEN_NUM, 0, 0, TOKEN_NUM, 0, 1, TOKEN_NUM, 1, 3 }
+    };
 
     PRINT_TEST_NAME();
 
-    value = decode_number(line_data_1, 0);
+    line_ptr = &line;
+    lp = offsetof(Line, data);
+
+    value = decode_number();
     ASSERT_EQ(value, 0);
 
-    value = decode_number(line_data_1, 3);
+    value = decode_number();
     ASSERT_EQ(value, 256);
 
-    value = decode_number(line_data_2, 0);
+    value = decode_number();
     ASSERT_EQ(value, 769);
 }
 
@@ -35,7 +51,7 @@ extern void* decode_xh_vectors[];
 static int num_count;
 
 static void xh_number(void) {
-    int value = decode_number(line_ptr, lp);
+    int value = decode_number();
     switch (++num_count) {
         case 1: ASSERT_EQ(value, 4112); break;
         case 2: ASSERT_EQ(value, 3); break;
@@ -76,7 +92,7 @@ static int paren_count;
 
 static void xh_paren(void) {
     ++paren_count;
-    decode_expression(decode_xh_vectors, line_ptr, lp);
+    decode_expression(decode_xh_vectors);
 }
 
 void* decode_xh_vectors[] = {
@@ -112,7 +128,10 @@ static void test_decode_expression(void) {
 
     PRINT_TEST_NAME();
 
-    decode_expression(decode_xh_vectors, &line, offsetof(Line, data));
+    line_ptr = &line;
+    lp = offsetof(Line, data);
+
+    decode_expression(decode_xh_vectors);
     ASSERT_EQ(num_count, 2);
     ASSERT_EQ(var_count, 3);
     ASSERT_EQ(op_count, 4);
