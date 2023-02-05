2fa8f205fe4b01750625c1b1ccf14cec070acac4
Willis Blackburn <wboyce@panix.com>
2023-02-05 17:15:45 -0500

    Save one byte in input.s

--- input.s
+++ input.s
@@ -24,7 +24,7 @@ exec_input:
         beq     @done                   ; It was TOKEN_NO_VALUE, nothing more to read
         jsr     parse_argument_separator    ; We read something from ths line so need a ',' to continue
         bcc     exec_input              ; Didn't find ',' so issue a new prompt
-        jmp     @next_var               ; Otherwise just read the next variable
+        bcs     @next_var               ; Otherwise just read the next variable
 
 @error:
         pla                             ; Remove variable from stack
