0e634e46016dd62917d96cb85b788311e0c3c663
Willis Blackburn <wboyce@panix.com>
2023-03-11 16:02:01 -0500

    Call parse_name to get name before calling parse_element

--- tables.s
+++ tables.s
@@ -23,6 +23,7 @@ statement_name_table:
         .byte   0
 
 operator_name_table:
+operator_chars:
         .byte   '+' | NT_END
         .byte   '-' | NT_END
         .byte   '*' | NT_END
@@ -30,11 +31,12 @@ operator_name_table:
         .byte   '^' | NT_END
         .byte   '&' | NT_END
         .byte   '=' | NT_END
+        .byte   '<' | NT_END
+        .byte   '>' | NT_END
+operator_chars_end:
         .byte   '<', '>' | NT_END
         .byte   '<', '=' | NT_END
-        .byte   '<' | NT_END
         .byte   '>', '=' | NT_END
-        .byte   '>' | NT_END
         .byte   'A', 'N', 'D' | NT_END
         .byte   'O', 'R' | NT_END
         .byte   0
