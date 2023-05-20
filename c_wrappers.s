5f4efd9eaffe59ae72bcdabdb1e87a774e1b25a2
Willis Blackburn <wboyce@panix.com>
2023-05-20 17:42:14 -0400

    Remove err

--- c_wrappers.s
+++ c_wrappers.s
@@ -172,7 +172,7 @@ _reset_line_ptr:
 _find_line_ax:
 .export _find_line_ax
         jsr     find_line_ax
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _advance_line_ptr:
 .export _advance_line_ptr
@@ -181,7 +181,7 @@ _advance_line_ptr:
 _insert_or_update_line:
 .export _insert_or_update_line
         jsr     insert_or_update_line
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _grow:
 .export _grow
@@ -190,7 +190,7 @@ _grow:
         tay                             ; Store in Y
         ldax    BC                      ; Get the size again
         jsr     grow
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _shrink:
 .export _shrink
@@ -199,8 +199,7 @@ _shrink:
         tay                             ; Store in Y
         ldax    BC                      ; Get the size again
         jsr     shrink
-        jmp     return_carry_flag
-        rts
+        jmp     set_carry_flag
 
 _calculate_bytes_to_move:
 .export _calculate_bytes_to_move
@@ -209,7 +208,7 @@ _calculate_bytes_to_move:
 _check_himem:
 .export _check_himem
         jsr     check_himem
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _set_variable_value_ptr:
 .export _set_variable_value_ptr
