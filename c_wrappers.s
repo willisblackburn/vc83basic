9fb1c2e79a0c12618482c9c59ab3f674b33ed6c6
Willis Blackburn <wboyce@panix.com>
2023-05-20 17:15:27 -0400

    Remove err

--- c_wrappers.s
+++ c_wrappers.s
@@ -121,22 +121,22 @@ _read_number:
 .export _read_number
         sta     bp                      ; Buffer index
         jsr     read_number
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _char_to_digit:
 .export _char_to_digit
         jsr     char_to_digit
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _parse_line:
 .export _parse_line
         jsr     parse_line
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _parse_statement:
 .export _parse_statement
         jsr     parse_statement
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _parse_directive:
 .export _parse_directive
@@ -145,7 +145,7 @@ _parse_directive:
         sta     bp
         jsr     popa
         jsr     parse_directive
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _parse_expression:
 .export _parse_expression
@@ -153,17 +153,17 @@ _parse_expression:
         jsr     popa
         sta     bp
         jsr     parse_expression
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _parse_name:
 .export _parse_name
         jsr     parse_name
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 _is_name_character:
 .export _is_name_character
         jsr     is_name_character
-        jmp     return_carry_flag
+        jmp     set_carry_flag
 
 ; program.s
 
--- parser_test.c
+++ parser_test.c
@@ -1,174 +1,171 @@
 #include "test.h"
 
  static void test_is_name_character(void) {
-    int err;
 
     PRINT_TEST_NAME();
 
-    err = is_name_character('A');
-    ASSERT_EQ(err, 0);
-    err = is_name_character('Z');
-    ASSERT_EQ(err, 0);
-    err = is_name_character('0');
-    ASSERT_EQ(err, 0);
-    err = is_name_character('9');
-    ASSERT_EQ(err, 0);
-    err = is_name_character('_');
-    ASSERT_EQ(err, 0);
+    is_name_character('A');
+    ASSERT_EQ(carry_flag, 0);
+    is_name_character('Z');
+    ASSERT_EQ(carry_flag, 0);
+    is_name_character('0');
+    ASSERT_EQ(carry_flag, 0);
+    is_name_character('9');
+    ASSERT_EQ(carry_flag, 0);
+    is_name_character('_');
+    ASSERT_EQ(carry_flag, 0);
 
-    err = is_name_character('?');
-    ASSERT_NE(err, 0);
-    err = is_name_character('=');
-    ASSERT_NE(err, 0);
-    err = is_name_character('#');
-    ASSERT_NE(err, 0);
-    err = is_name_character('%');
-    ASSERT_NE(err, 0);
-    err = is_name_character('@');
-    ASSERT_NE(err, 0);
-    err = is_name_character('[');
-    ASSERT_NE(err, 0);
-    err = is_name_character('/');
-    ASSERT_NE(err, 0);
-    err = is_name_character(':');
-    ASSERT_NE(err, 0);
-    err = is_name_character(' ');
-    ASSERT_NE(err, 0);
-    err = is_name_character(0);
-    ASSERT_NE(err, 0);
-    err = is_name_character(0x7F);
-    ASSERT_NE(err, 0);
-    err = is_name_character(0x80);
-    ASSERT_NE(err, 0);
-    err = is_name_character(0xFF);
-    ASSERT_NE(err, 0);
+    is_name_character('?');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character('=');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character('#');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character('%');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character('@');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character('[');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character('/');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character(':');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character(' ');
+    ASSERT_NE(carry_flag, 0);
+    is_name_character(0);
+    ASSERT_NE(carry_flag, 0);
+    is_name_character(0x7F);
+    ASSERT_NE(carry_flag, 0);
+    is_name_character(0x80);
+    ASSERT_NE(carry_flag, 0);
+    is_name_character(0xFF);
+    ASSERT_NE(carry_flag, 0);
 }
 
- static char call_parse_name(const char* s, char set_bp) {
+ static void call_parse_name(const char* s, char set_bp) {
     strcpy(buffer, s);
     bp = set_bp;
-    return parse_name();
+    parse_name();
 }
 
 static void test_parse_name(void) {
-    char err;
 
     PRINT_TEST_NAME();
 
-    err = call_parse_name("PRINT", 0);
-    ASSERT_EQ(err, 0);
+    call_parse_name("PRINT", 0);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 0);
     ASSERT_EQ(bp, 5);
 
     // Start at the space to verify that it skips whitespace.
-    err = call_parse_name("10 PRINT", 2);
-    ASSERT_EQ(err, 0);
+    call_parse_name("10 PRINT", 2);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 3);
     ASSERT_EQ(bp, 8);
 
-    err = call_parse_name("10 PRINT X", 3);
-    ASSERT_EQ(err, 0);
+    call_parse_name("10 PRINT X", 3);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 3);
     ASSERT_EQ(bp, 8);
 
-    err = call_parse_name("10 PRINTX", 3);
-    ASSERT_EQ(err, 0);
+    call_parse_name("10 PRINTX", 3);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 3);
     ASSERT_EQ(bp, 9);
 
-    err = call_parse_name("10 PRINT10", 3);
-    ASSERT_EQ(err, 0);
+    call_parse_name("10 PRINT10", 3);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 3);
     ASSERT_EQ(bp, 10);
 
-    err = call_parse_name("10 PRINT10X", 3);
-    ASSERT_EQ(err, 0);
+    call_parse_name("10 PRINT10X", 3);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 3);
     ASSERT_EQ(bp, 11);
 
     // Start parse at space.
-    err = call_parse_name("ON X/2 GOTO 10,20", 2);
-    ASSERT_EQ(err, 0);
+    call_parse_name("ON X/2 GOTO 10,20", 2);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 3);
     ASSERT_EQ(bp, 4);
-    err = call_parse_name("ON X/2 GOTO 10,20", 6);
-    ASSERT_EQ(err, 0);
+    call_parse_name("ON X/2 GOTO 10,20", 6);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 7);
     ASSERT_EQ(bp, 11);
 
     // Digits are names; this is okay because we try to parse numbers before names.
-    err = call_parse_name("10", 0);
-    ASSERT_EQ(err, 0);
+    call_parse_name("10", 0);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(name_bp, 0);
     ASSERT_EQ(bp, 2);
 
     // bp will reflect skipped whitespace even if parse fails.
-    err = call_parse_name("   ", 0);
-    ASSERT_NE(err, 0);
+    call_parse_name("   ", 0);
+    ASSERT_NE(carry_flag, 0);
     ASSERT_EQ(bp, 3);
 
-    err = call_parse_name("", 0);
-    ASSERT_NE(err, 0);
+    call_parse_name("", 0);
+    ASSERT_NE(carry_flag, 0);
     ASSERT_EQ(bp, 0);
 }
 
 static void test_char_to_digit(void) {
-    int err;
+    char d;
 
     PRINT_TEST_NAME();
 
-    err = char_to_digit('0');
-    ASSERT_EQ(err, 0);
-    ASSERT_EQ(reg_a, 0);
-    err = char_to_digit('9');
-    ASSERT_EQ(err, 0);
-    ASSERT_EQ(reg_a, 9);
-    err = char_to_digit('0'-1);
-    ASSERT_NE(err, 0);
-    err = char_to_digit('9'+1);
-    ASSERT_NE(err, 0);
-    err = char_to_digit(' ');
-    ASSERT_NE(err, 0);
-    err = char_to_digit('A');
-    ASSERT_NE(err, 0);
-    err = char_to_digit(0);
-    ASSERT_NE(err, 0);
-    err = char_to_digit(255);
-    ASSERT_NE(err, 0);
+    d = char_to_digit('0');
+    ASSERT_EQ(carry_flag, 0);
+    ASSERT_EQ(d, 0);
+    d = char_to_digit('9');
+    ASSERT_EQ(carry_flag, 0);
+    ASSERT_EQ(d, 9);
+    char_to_digit('0'-1);
+    ASSERT_NE(carry_flag, 0);
+    char_to_digit('9'+1);
+    ASSERT_NE(carry_flag, 0);
+    char_to_digit(' ');
+    ASSERT_NE(carry_flag, 0);
+    char_to_digit('A');
+    ASSERT_NE(carry_flag, 0);
+    char_to_digit(0);
+    ASSERT_NE(carry_flag, 0);
+    char_to_digit(255);
+    ASSERT_NE(carry_flag, 0);
 }
 
 static void test_read_number(void) {
-    int err;
+    int number;
 
     PRINT_TEST_NAME();
 
     strcpy(buffer, "10 PRINT X");
-    err = read_number(0);
-    ASSERT_EQ(err, 0);
-    ASSERT_EQ(reg_ax, 10);
+    number = read_number(0);
+    ASSERT_EQ(carry_flag, 0);
+    ASSERT_EQ(number, 10);
     ASSERT_EQ(bp, 2);
 
     // The function should honor the current read position.
     strcpy(buffer, "1020 PRINT X");
-    err = read_number(2);
-    ASSERT_EQ(err, 0);
-    ASSERT_EQ(reg_ax, 20);
+    number = read_number(2);
+    ASSERT_EQ(carry_flag, 0);
+    ASSERT_EQ(number, 20);
     ASSERT_EQ(bp, 4);
 
     // The function should return carry set if an invalid number.
     strcpy(buffer, "invalid");
-    err = read_number(0);
-    ASSERT_NE(err, 0);
+    read_number(0);
+    ASSERT_NE(carry_flag, 0);
     ASSERT_EQ(bp, 0);
 
     strcpy(buffer, "");
-    err = read_number(0);
-    ASSERT_NE(err, 0);
+    read_number(0);
+    ASSERT_NE(carry_flag, 0);
     ASSERT_EQ(bp, 0);
 }
 
 static void test_parse_expression(void) {
-    int err;
     
     const char line_data_1[] = { TOKEN_NUM, 0x01, 0x00 };
     const char line_data_2[] = { 0x80 };
@@ -178,15 +175,15 @@ static void test_parse_expression(void) {
     initialize_program();
 
     strcpy(buffer, "1");
-    err = parse_expression(0, offsetof(Line, data));
-    ASSERT_EQ(err, 0);
+    parse_expression(0, offsetof(Line, data));
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_1, sizeof line_data_1);
     ASSERT_EQ(bp, 1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_1);
 
     strcpy(buffer, "X");
-    err = parse_expression(0, offsetof(Line, data));
-    ASSERT_EQ(err, 0);
+    parse_expression(0, offsetof(Line, data));
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_2, sizeof line_data_2);
     ASSERT_EQ(bp, 1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_2);
@@ -195,7 +192,6 @@ static void test_parse_expression(void) {
 }
 
 static void test_parse_directive(void) {
-    int err;
 
     const char line_data_1[] = { TOKEN_NUM, 0x01, 0x00 };
     const char line_data_2[] = { 0x80 };
@@ -205,22 +201,22 @@ static void test_parse_directive(void) {
     initialize_program();
 
     strcpy(buffer, "1");
-    err = parse_directive(1, 0, offsetof(Line, data));
-    ASSERT_EQ(err, 0);
+    parse_directive(1, 0, offsetof(Line, data));
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_1, sizeof line_data_1);
     ASSERT_EQ(bp, 1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_1);
 
     strcpy(buffer, "X");
-    err = parse_directive(1, 0, offsetof(Line, data));
-    ASSERT_EQ(err, 0);
+    parse_directive(1, 0, offsetof(Line, data));
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_2, sizeof line_data_2);
     ASSERT_EQ(bp, 1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_2);
 
     strcpy(buffer, "X");
-    err = parse_directive(NT_VAR, 0, offsetof(Line, data));
-    ASSERT_EQ(err, 0);
+    parse_directive(NT_VAR, 0, offsetof(Line, data));
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_2, sizeof line_data_2);
     ASSERT_EQ(bp, 1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_2);
@@ -228,19 +224,18 @@ static void test_parse_directive(void) {
 
 static void call_parse_statement(const char* s, const char* expect_line_data, size_t expect_line_data_length,
     int line) {
-    char err;
     fprintf(stderr, "  %s:%d: parse_statement(\"%s\")\n", __FILE__, line, s);
     strcpy(buffer, s);
     bp = 0;
     lp = offsetof(Line, data);
-    err = parse_statement(statement_name_table);
-    ASSERT_EQ(err, 0);
+    parse_statement(statement_name_table);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, expect_line_data, expect_line_data_length);
     ASSERT_EQ(lp, offsetof(Line, data) + expect_line_data_length);
 }
 
 static void test_parse_statement(void) {
-    int err;
+
     const char line_data_1[] = { ST_RUN };
     const char line_data_2[] = { ST_PRINT, TOKEN_NUM, 0x08, 0x00 };
     const char line_data_3[] = { ST_LET, 0x80, TOKEN_NUM, 0x64, 0x00 };
@@ -264,14 +259,13 @@ static void test_parse_statement(void) {
     strcpy(buffer, "PRINTX");
     bp = 0;
     lp = offsetof(Line, data);
-    err = parse_statement(statement_name_table);
-    ASSERT_NE(err, 0);
+    parse_statement(statement_name_table);
+    ASSERT_NE(carry_flag, 0);
     ASSERT_EQ(bp, 6);
     ASSERT_EQ(lp, 3);
 }
 
 static void test_parse_line(void) {
-    int err;
 
     const char line_data_1[] = { ST_LET, 0x80, TOKEN_NUM, 0x64, 0x00 };
     const char line_data_2[] = { ST_RUN };
@@ -283,8 +277,8 @@ static void test_parse_line(void) {
     // Happy path with line number
 
     strcpy(buffer, "10 LET X=100");
-    err = parse_line();
-    ASSERT_EQ(err, 0);
+    parse_line();
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(line_buffer.next_line_offset, offsetof(Line, data) + sizeof line_data_1);
     ASSERT_EQ(line_buffer.number, 10);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_1, sizeof line_data_1);
@@ -292,8 +286,8 @@ static void test_parse_line(void) {
     // Happy path immediate mode
 
     strcpy(buffer, "RUN");
-    err = parse_line();
-    ASSERT_EQ(err, 0);
+    parse_line();
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_EQ(line_buffer.next_line_offset,  offsetof(Line, data) + sizeof line_data_2);
     ASSERT_EQ(line_buffer.number, -1);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_2, sizeof line_data_2);
@@ -301,17 +295,17 @@ static void test_parse_line(void) {
     // Empty line
 
     strcpy(buffer, "");
-    err = parse_line();
-    ASSERT_EQ(err, 0);
+    parse_line();
+    ASSERT_EQ(carry_flag, 0);
     strcpy(buffer, "  ");
-    err = parse_line();
-    ASSERT_EQ(err, 0);
+    parse_line();
+    ASSERT_EQ(carry_flag, 0);
 
     // Test that the parser rejects statements that continue past the point where they're supposed to end.
 
     strcpy(buffer, "LET X=100,5");
-    err = parse_line();
-    ASSERT_NE(err, 0);
+    parse_line();
+    ASSERT_NE(carry_flag, 0);
 }
 
 int main(void) {
