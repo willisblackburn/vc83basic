b41e2e2b12ad2ad223fd4e62a6f587c569317c11
Willis Blackburn <wboyce@panix.com>
2023-05-20 18:20:34 -0400

    Update backport

--- backport.sh
+++ backport.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Backport the latest commit on the current branch to the previous branch.
-# The "previous" branch is hard-coded in this script; in each branch the "previous" branch is different.
-# Since the script itself will be replaced when running "git checkout," we do that in a subshell and then have the
-# subshell re-run this script after the checkout.
+# There's no previous branch from here.
 
-PREVIOUS_BRANCH=e13
-
-LATEST_COMMIT=$(git rev-parse HEAD)
-echo Backporting $LATEST_COMMIT to $PREVIOUS_BRANCH
-
-/bin/bash -c "git checkout $PREVIOUS_BRANCH; git cherry-pick $LATEST_COMMIT; git push; $0"
+echo Backport complete.
