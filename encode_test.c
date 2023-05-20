d0628562231702a1a008bcd67ed20c49e804f0b6
Willis Blackburn <wboyce@panix.com>
2023-05-20 18:49:09 -0400

    Remove err

--- encode_test.c
+++ encode_test.c
@@ -1,7 +1,6 @@
 #include "test.h"
 
 static void test_encode_byte(void) {
-    int err;
 
     const char line_data_1[] = { 0x02 };
     const char line_data_2[] = { 0x02, 0x03 };
@@ -10,30 +9,29 @@ static void test_encode_byte(void) {
     PRINT_TEST_NAME();
 
     lp = offsetof(Line, data);
-    err = encode_byte(2);
-    ASSERT_EQ(err, 0);
+    encode_byte(2);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_1, sizeof line_data_1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_1);
 
-    err = encode_byte(3);
-    ASSERT_EQ(err, 0);
+    encode_byte(3);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_2, sizeof line_data_2);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_2);
 
     lp = offsetof(Line, data);
-    err = encode_byte(255);
-    ASSERT_EQ(err, 0);
+    encode_byte(255);
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_3, sizeof line_data_3);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_3);
 
     // Encode at end of buffer should fail
     lp = 255;
-    err = encode_byte(100);
-    ASSERT_NE(err, 0);
+    encode_byte(100);
+    ASSERT_NE(carry_flag, 0);
 }
 
 static void test_encode_number(void) {
-    int err;
 
     const char line_data_1[] = { TOKEN_NUM, 0x00, 0x00, 0x00, 0x00, 0 };
     const char line_data_2[] = { TOKEN_NUM, 0x00, 0x00, 0x00, 0x48, 133,
@@ -44,33 +42,33 @@ static void test_encode_number(void) {
 
     SET_FPX(FP0, POSITIVE, 1, 0);
     lp = offsetof(Line, data);
-    err = encode_number();
-    ASSERT_EQ(err, 0);
+    encode_number();
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_1, sizeof line_data_1);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_1);
 
     // 100
     SET_FPX(FP0, POSITIVE, 133, 0xC8000000);
     lp = offsetof(Line, data);
-    err = encode_number();
-    ASSERT_EQ(err, 0);
+    encode_number();
+    ASSERT_EQ(carry_flag, 0);
 
     // 4112
     SET_FPX(FP0, POSITIVE, 139, 0x80800000);
-    err = encode_number();
-    ASSERT_EQ(err, 0);
+    encode_number();
+    ASSERT_EQ(carry_flag, 0);
 
     // 3.14159
     SET_FPX(FP0, POSITIVE, 128, 0xC90FCF81);
-    err = encode_number();
-    ASSERT_EQ(err, 0);
+    encode_number();
+    ASSERT_EQ(carry_flag, 0);
     ASSERT_MEMORY_EQ(line_buffer.data, line_data_2, sizeof line_data_2);
     ASSERT_EQ(lp, offsetof(Line, data) + sizeof line_data_2);
 
     // Encode at end of buffer should fail (doesn't matter what FP0 is)
     lp = 252;
-    err = encode_number();
-    ASSERT_NE(err, 0);
+    encode_number();
+    ASSERT_NE(carry_flag, 0);
 }
 
 int main(void) {
