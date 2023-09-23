c9413652764f8d6e2f152232ab36909119a510d2
Willis Blackburn <wboyce@panix.com>
2023-09-23 11:20:42 -0400

    Improve error handling

--- list.s
+++ list.s
@@ -76,6 +76,9 @@ list_statement:
 
 list_name:
         jsr     get_name_table_entry
+        jsr     append_from_name_table_entry
+        clc                             ; Signal success
+        rts
 
 ; Outputs characters from the name table entry starting at np, until reaching the last character or a
 ; directive.
@@ -182,12 +185,15 @@ list_repeated_variable:
         lda     (line_ptr),y
         bne     loop_list_repeated_variable ; Not TOKEN_NO_VALUE so keep going
         inc     lp                      ; Skip over TOKEN_NO_VALUE
+        clc                             ; Signal success
         rts
 
 list_number:
         jsr     add_whitespace
         jsr     decode_number           ; Decode the number
-        jmp     format_number           ; Send it right to format_number
+        jsr     format_number           ; Send it right to format_number
+        clc                             ; Signal success
+        rts
 
 list_operator:
         jsr     decode_operator
@@ -209,7 +215,9 @@ list_paren:
         ldax    #list_vectors
         jsr     decode_expression
         lda     #')'
-        jmp     append_buffer
+        jsr     append_buffer
+        clc                             ; Signal success
+        rts
 
 ; Adds whitespace to the output if necessary.
 ; Whitespace is necessary if bp > 0 and if buffer[bp-1] is a name character or is a ')'.
