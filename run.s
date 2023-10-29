1014c7202beccef9ad39ee74afe04d6dbfc5ecf5
Willis Blackburn <wboyce@panix.com>
2023-10-29 14:49:47 -0400

    Fix vector bug

--- run.s
+++ run.s
@@ -30,10 +30,10 @@ run_line:
         rts
 
 statement_exec_vectors:
-        .word   exec_run
-        .word   exec_print
-        .word   exec_let
-        .word   exec_input
+        .word   exec_run-1
+        .word   exec_print-1
+        .word   exec_let-1
+        .word   exec_input-1
 
 ; Invokes a statement handler from a table.
 ; This function does not return; it jumps to the handler, which will eventually return.
