88dcaf04a9d91a7800cbb809bedaf6453abf18be
Willis Blackburn <wboyce@panix.com>
2023-11-18 10:35:57 -0500

    Don't need assert in e11-1

--- control.s
+++ control.s
@@ -4,9 +4,6 @@
 ; Logic depends on TOKEN_NO_VALUE being zero
 .assert TOKEN_NO_VALUE = 0, error
 
-; primary_stack must be page-aligned
-.assert <primary_stack = 0, error
-
 ; GOTO statement:
 
 exec_goto:
