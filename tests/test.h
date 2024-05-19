e9fbf23e587bec7a8e5c8ba7921da73bd3ebbefd
Willis Blackburn <wboyce@panix.com>
2024-05-19 14:35:23 -0400

    Update test args.

--- tests/test.h
+++ tests/test.h
@@ -84,7 +84,7 @@ extern const char statement_name_table[];
 // Prototypes for C wrapper functions
 
 // decode.s
-void decode_expression(void** vector_table_ptr);
+void decode_expression(/* AX */ void** vector_table_ptr);
 void decode_number(void);
 const char* decode_string(void);
 char decode_variable(void);
@@ -96,10 +96,10 @@ char decode_byte(void);
 void evaluate_expression(void);
 void push_fp0(void);
 void pop_fp0(void);
-void push_string(const String* value);
+void push_string(/* AX */ const String* value);
 const String* pop_string(void);
-char stack_alloc(char size);
-void stack_free(char size);
+char stack_alloc(/* AX */ char size);
+void stack_free(/* AX */ char size);
 
 // encode.s
 void encode_number(void);
@@ -128,7 +128,7 @@ int fcmp(void);
 // list.s
 void list_line(void);
 void list_statement(void);
-void list_directive(char directive);
+void list_directive(/* A */ char directive);
 
 // name.s
 char find_name(/* AX */ const char* name_ptr);
@@ -144,7 +144,7 @@ void parse_argument_separator(void);
 void parse_name(void);
 void is_name_character(/* A */ char c);
 void parse_operator_name();
-void is_operator_name_character(char c, char index);
+void is_operator_name_character(/* A */ char c, /* Y */ char index);
 
 // program.s
 void initialize_target(void);
