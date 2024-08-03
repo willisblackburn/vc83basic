300343dc0efa949f4b11e6e5eb09e76364f52fff
Willis Blackburn <wboyce@panix.com>
2024-08-03 09:07:25 -0400

    Eliminate statement table end labels

--- tables.s
+++ tables.s
@@ -1,25 +1,20 @@
 .include "basic.inc"
 
 statement_name_table:
-        .byte   run_end - *, 'R', 'U', 'N' | NT_STOP
-run_end:
-        .byte   print_end - *, 'P', 'R', 'I', 'N', 'T' | NT_STOP, 1
-print_end:
-        .byte   let_end - *, 'L', 'E', 'T' | NT_STOP, NT_VAR, '=', 1
-let_end:
-        .byte   input_end - *, 'I', 'N', 'P', 'U', 'T' | NT_STOP, NT_RPT_VAR
-input_end:
-        .byte   list_end - *, 'L', 'I', 'S', 'T' | NT_STOP, 2
-list_end:
-        .byte   0
+        .byte   :+ - *, 'R', 'U', 'N' | NT_STOP
+:       .byte   :+ - *, 'P', 'R', 'I', 'N', 'T' | NT_STOP, 1
+:       .byte   :+ - *, 'L', 'E', 'T' | NT_STOP, NT_VAR, '=', 1
+:       .byte   :+ - *, 'I', 'N', 'P', 'U', 'T' | NT_STOP, NT_RPT_VAR
+:       .byte   :+ - *, 'L', 'I', 'S', 'T' | NT_STOP, 2
+:       .byte   0
 
 operator_name_table:
-        .byte   2, '+' | NT_STOP
-        .byte   2, '-' | NT_STOP
-        .byte   2, '*' | NT_STOP
-        .byte   2, '/' | NT_STOP
-        .byte   2, '^' | NT_STOP
-        .byte   0
+        .byte   :+ - *, '+' | NT_STOP
+:       .byte   :+ - *, '-' | NT_STOP
+:       .byte   :+ - *, '*' | NT_STOP
+:       .byte   :+ - *, '/' | NT_STOP
+:       .byte   :+ - *, '^' | NT_STOP
+:       .byte   0
 
 ; Operator precedence table
 ; We index this by the operator index divided by 2.
@@ -30,5 +25,5 @@ operator_precedence_table:
         .byte   PR_POW          ; ^
 
 unary_operator_name_table:
-        .byte   2, '-' | NT_STOP
-        .byte   0
+        .byte   :+ - *, '-' | NT_STOP
+:       .byte   0
