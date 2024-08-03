c5197878953b9d9847222970e3424d68b6165d05
Willis Blackburn <wboyce@panix.com>
2024-08-03 09:05:09 -0400

    Eliminate statement table end labels

--- tables.s
+++ tables.s
@@ -1,14 +1,9 @@
 .include "basic.inc"
 
 statement_name_table:
-        .byte   run_end - *, 'R', 'U', 'N' | NT_STOP
-run_end:
-        .byte   print_end - *, 'P', 'R', 'I', 'N', 'T' | NT_STOP, 1
-print_end:
-        .byte   let_end - *, 'L', 'E', 'T' | NT_STOP, NT_VAR, '=', 1
-let_end:
-        .byte   input_end - *, 'I', 'N', 'P', 'U', 'T' | NT_STOP, NT_RPT_VAR
-input_end:
-        .byte   list_end - *, 'L', 'I', 'S', 'T' | NT_STOP, 2
-list_end:
-        .byte   0
+        .byte   :+ - *, 'R', 'U', 'N' | NT_STOP
+:       .byte   :+ - *, 'P', 'R', 'I', 'N', 'T' | NT_STOP, 1
+:       .byte   :+ - *, 'L', 'E', 'T' | NT_STOP, NT_VAR, '=', 1
+:       .byte   :+ - *, 'I', 'N', 'P', 'U', 'T' | NT_STOP, NT_RPT_VAR
+:       .byte   :+ - *, 'L', 'I', 'S', 'T' | NT_STOP, 2
+:       .byte   0
