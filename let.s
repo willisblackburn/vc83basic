38ca40d5e88fa8e094b4a7cb7c4012d58a843ea6
Willis Blackburn <wboyce@panix.com>
2024-08-10 11:27:32 -0400

    Crate find_or_add_variable in name.s

--- let.s
+++ let.s
@@ -5,12 +5,8 @@
 
 exec_let:
         jsr     decode_name             ; Sets name_ptr and name_length
-        ldax    variable_name_table_ptr
-        jsr     find_name               ; Look for a variable with this name
-        bcc     @found                  ; Found it
-        ldax    #2                      ; Allocate 2 bytes of space for the variable
-        jsr     add_variable            ; Add it
-        bcs     @error                  ; Unable to add the variable
+        jsr     find_or_add_variable
+        bcs     @error
 
 ; At this point record_ptr will be pointing to the variable data.
 ; Store it in variable_ptr because we might need record_ptr when parsing the right hand value.
