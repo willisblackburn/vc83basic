6d6cc71749498b9eb671d54d25054e65752de5d5
Willis Blackburn <wboyce@panix.com>
2024-08-18 12:12:35 -0400

    Simplify evaluate_expression

--- expression.s
+++ expression.s
@@ -14,14 +14,8 @@ evaluate_expression:
 
 @variable:
         jsr     decode_name
-        ldax    variable_name_table_ptr
-        jsr     find_name               ; Look for a variable with this name
-        bcc     @found                  ; Found it
-        ldax    #2                      ; Allocate 2 bytes of space for the variable
-        jsr     add_variable            ; Add it
-        bcs     @error                  ; Unable to add the variable
-@found:
-        ldax    record_ptr
+        jsr     find_or_add_variable
+        bcs     @error
         ldy     #1                      ; Start with high byte of value
         lda     (record_ptr),y
         tax
