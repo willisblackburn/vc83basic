c855ea91f5a1648c85e1cc3826c3697701c28e45
Willis Blackburn <wboyce@panix.com>
2024-11-17 11:25:43 -0500

    Everything seems to work so far

--- input.s
+++ input.s
@@ -37,36 +37,10 @@ exec_input:
         rts
 
 @string:
-        lda     #0                      ; Allocate 0-byte string
-        jsr     string_alloc            ; Don't care about the address of this string
-        bcs     @done
-        lda     #255                    ; Allocate second 255-byte string
-        jsr     string_alloc
-        bcs     @done
-        stax    dst_ptr                 ; Write the string here
-        mva     #0, di                  ; Length byte at index 0
-        mvax    #buffer, src_ptr        ; Read from buffer
-        mva     buffer_pos, si
+        ldax    #buffer
+        ldy     buffer_pos
         jsr     read_string
         bcs     @done
-        mva     si, buffer_pos          ; Update buffer_pos to move past string
-        lda     di                      ; Add di + 3 to dst_ptr and store in BC
-        ldx     #0                      ; High byte of 3
-        adc     #3                      ; Carry is already clear
-        bne     @skip_inx               ; No carry
-        inx
-@skip_inx:
-        clc
-        adc     dst_ptr
-        sta     B
-        txa
-        adc     dst_ptr+1
-        sta     C
-        sec
-        lda     #255                    ; Calculate length of unused remainder string
-        sbc     di
-        ldy     #0                      ; Length byte
-        sta     (BC),y                  ; Update length byte
-        ldax    dst_ptr                 ; Address of the string we just read
-        jsr     push_string             ; Push it onto stack
+        sty     buffer_pos              ; Update buffer_pos to next read position
+        jsr     push_string             ; Push result string onto the stack
         jmp     @assign
