56caeaa2cda6cb5720450e417b14658e02195ae3
Willis Blackburn <wboyce@panix.com>
2024-11-29 18:20:58 -0500

    Remove TOKEN_NO_VALUE in comments #165

--- input.s
+++ input.s
@@ -11,22 +11,20 @@ exec_input:
 @next_var:
         jsr     decode_name             ; Read the variable name
         jsr     find_or_add_variable
-        bcs     @error
+        bcs     @done
         mvax    name_ptr, variable_ptr
         ldax    #buffer
         ldy     buffer_pos
         jsr     read_number
-        bcs     @error
+        bcs     @done
         sty     buffer_pos              ; Update buffer_pos
         jsr     assign_variable
         ldy     line_pos                ; Peek at the next byte
         lda     (line_ptr),y
+        clc                             ; Clear carry in case we're done            
         beq     @done                   ; It was 0, nothing more to read
         jsr     parse_argument_separator    ; We read something from ths line so need a ',' to continue
         bcc     exec_input              ; Didn't find ',' so issue a new prompt
         bcs     @next_var               ; Otherwise just read the next variable
-
 @done:
-        clc
-@error:
         rts
