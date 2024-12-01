3d1a35f3132e5949393252c70a7c212cbe38404c
Willis Blackburn <wboyce@panix.com>
2024-11-30 23:22:28 -0500

    XH_NUM -> XH_NUMBER

--- list.s
+++ list.s
@@ -192,7 +192,7 @@ list_argument_list:
 list_vectors:
         .word   list_unary_operator-1   ; XH_UNARY_OP
         .word   list_operator-1         ; XH_OP
-        .word   list_number-1           ; XH_NUM
+        .word   list_number-1           ; XH_NUMBER
         .word   list_variable-1         ; XH_VAR
         .word   list_paren-1            ; XH_PAREN
 
