50320ffd42c53c077f4996717e9847cb5e049117
Willis Blackburn <wboyce@panix.com>
2025-01-05 12:07:19 -0500

    Eliminate variable_ptr and variable_type

--- input.s
+++ input.s
@@ -12,7 +12,6 @@ exec_input:
         jsr     decode_name             ; Read the variable name
         jsr     find_or_add_variable
         bcs     @done
-        mvax    name_ptr, variable_ptr  ; Set up target for assign_variable
         ldax    #buffer                 ; Point to buffer
         ldy     buffer_pos              ; Starting at buffer_pos
         jsr     string_to_fp            ; Parse the number
--- let.s
+++ let.s
@@ -5,24 +5,22 @@
 
 exec_let:
         jsr     decode_name             ; Sets decode_name_ptr and decode_name_length
+        phzp    DECODE_NAME_STATE, DECODE_NAME_STATE_SIZE   ; Remember the decoded name
+        jsr     evaluate_expression     ; Value is now on the evaluation stack
+        plzp    DECODE_NAME_STATE, DECODE_NAME_STATE_SIZE   ; Recover the decoded name
+        bcs     @error
         jsr     find_or_add_variable
         bcs     @error
-
-; At this point name_ptr will be pointing to the variable data.
-; Store it in variable_ptr because we might need name_ptr when parsing the right hand value.
-
-        mvax    name_ptr, variable_ptr
-        jsr     evaluate_expression
         jmp     assign_variable
 
 @error:
         rts
 
-; Pops a value from the stack and copies it into the variable identified by variable_ptr.
-; variable_ptr = pointer to the variable's data in the variable name table
+; Pops a value from the stack and copies it into the variable identified by name_ptr.
+; name_ptr = pointer to the variable's data in the variable name table
 
 assign_variable:
-        mvax    variable_ptr, dst_ptr   ; Copy into variable data
+        mvax    name_ptr, dst_ptr       ; Copy into variable data
         lda     stack_pos               ; Get stack pointer
         ldx     #>stack                 ; Stack page
         ldy     #.sizeof(Float)
