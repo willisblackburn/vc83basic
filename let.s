917d28538dfee8c0f8979768e75922b467cdefbc
Willis Blackburn <wboyce@panix.com>
2025-01-05 12:07:19 -0500

    Eliminate variable_ptr and variable_type

--- let.s
+++ let.s
@@ -5,20 +5,28 @@
 
 exec_let:
         jsr     decode_name             ; Sets decode_name_ptr and decode_name_length
+        phzp    DECODE_NAME_STATE, DECODE_NAME_STATE_SIZE   ; Remember the decoded name
+        jsr     evaluate_expression     ; Value is now on the evaluation stack
+        plzp    DECODE_NAME_STATE, DECODE_NAME_STATE_SIZE   ; Recover the decoded name
+        bcs     @error
         jsr     find_or_add_variable
         bcs     @error
-
-; At this point name_ptr will be pointing to the variable data.
-; Store it in variable_ptr because we might need name_ptr when parsing the right hand value.
-
-@found:
-        mvax    name_ptr, variable_ptr
         jsr     evaluate_expression     ; Value is in AX
+        jmp     assign_variable
+
+@error:
+        rts
+
+; Assigns the value in AX to the variable identified by name_ptr.
+; AX = the variable value
+; name_ptr = pointer to the variable's data in the variable name table
+
+assign_variable:
         ldy     #0                      ; Index variable value with Y
-        sta     (variable_ptr),y        ; Low byte
+        sta     (name_ptr),y            ; Low byte
         iny
         txa
-        sta     (variable_ptr),y        ; High byte
+        sta     (name_ptr),y            ; High byte
         clc                             ; Success
 @error:
         rts
