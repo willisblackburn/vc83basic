6dc30fe539d5363a24fbfa12e52ab2be1935293e
Willis Blackburn <wboyce@panix.com>
2025-01-05 12:54:40 -0500

    Fix LET issue following elimination of variable_ptr

--- let.s
+++ let.s
@@ -5,13 +5,12 @@
 
 exec_let:
         jsr     decode_name             ; Sets decode_name_ptr and decode_name_length
-        phzp    DECODE_NAME_STATE, DECODE_NAME_STATE_SIZE   ; Remember the decoded name
-        jsr     evaluate_expression     ; Value is now on the evaluation stack
-        plzp    DECODE_NAME_STATE, DECODE_NAME_STATE_SIZE   ; Recover the decoded name
-        bcs     @error
         jsr     find_or_add_variable
-        bcs     @error
+        ldphaa  name_ptr                ; Remember name_ptr
         jsr     evaluate_expression     ; Value is in AX
+        tay                             ; Hold low byte of result while we recover name_ptr
+        plstaa  name_ptr                ; Recover name_ptr
+        tya                             ; Restore low byte of result
         jmp     assign_variable
 
 @error:
