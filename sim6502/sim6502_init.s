5286f74522c099fc32a411966b6dbff6a31b4384
Willis Blackburn <wboyce@panix.com>
2025-04-06 20:06:00 -0400

    Fix longstanding issue with debug format string

--- sim6502/sim6502_init.s
+++ sim6502/sim6502_init.s
@@ -47,7 +47,7 @@ flag_indicators: .res 8
 
 .code
 
-format: .byte "$%02X: A=%02X X=%02X Y=%02X BCDE=%08LX SP=%02X %.8s FPX:FP0t=%08LX:%08LX e=%02X s=%02X FP1t=%08LX:%08LX e=%02X s=%02X src_ptr=%04X dst_ptr=%04X", $0A, $00
+format: .byte "$%02X: A=%02X X=%02X Y=%02X BCDE=%08LX SP=%02X %.8s FPX:FP0t=%08LX:%08LX e=%02X s=%02X FP1t=%08LX e=%02X s=%02X src_ptr=%04X dst_ptr=%04X", $0A, $00
 flag_names: .byte "NV-BDIZC"
 
 ; Prints the register values to stderr.
