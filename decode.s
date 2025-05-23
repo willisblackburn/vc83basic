47f4c1d1a78102b7f00934acd5685d31892dd3e1
Willis Blackburn <wboyce@panix.com>
2025-05-23 00:17:12 -0400

    Pretty sure this should have been there all along

--- decode.s
+++ decode.s
@@ -50,6 +50,7 @@ decode_expression:
         cmp     #<('"'- 'A')
         beq     @dispatch
         iny                             ; Variable
+        cmp     #26                     ; Is it one of 26 letters starting with 'A'?
         bcc     @dispatch
         iny                             ; Subexpression start
         cmp     #<('(' - 'A')
