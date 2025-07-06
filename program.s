8085af9a42650f8485b76ce334fa9892d14ce5b7
Willis Blackburn <wboyce@panix.com>
2025-07-06 15:51:23 -0400

    Save two bytes

--- program.s
+++ program.s
@@ -44,8 +44,7 @@ reset_program_state:
         lda     #0                      ; Load zero into A
         tay                             ; Write index is also zero
         sta     (variable_name_table_ptr),y ; Initialize variable name table to 0
-
-; Fall through
+        rts
 
 ; Sets line_ptr to program_ptr.
 ; Returns line_ptr in AX.
