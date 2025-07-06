b8f0f9a6f42fdab913d54c26b9ead82d57d3c150
Willis Blackburn <wboyce@panix.com>
2025-07-06 10:25:04 -0400

    Don't add END statement and instead add a zero-length line.

--- tables.s
+++ tables.s
@@ -1,7 +1,7 @@
 .include "basic.inc"
 
 statement_name_table:
-:       .byte   :+ - *, 'R', 'U', 'N' | EOT
+        .byte   :+ - *, 'R', 'U', 'N' | EOT
 :       .byte   :+ - *, 'P', 'R', 'I', 'N', 'T' | EOT, 1
 :       .byte   :+ - *, 'L', 'E', 'T' | EOT, NT_VAR, '=', 1
 :       .byte   0
