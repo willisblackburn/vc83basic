9a7c671baaff045c3a7257883e66d7a1556e31c1
Willis Blackburn <wboyce@panix.com>
2025-09-28 14:44:21 -0400

    Fix semis

--- list.s
+++ list.s
@@ -136,7 +136,7 @@ list_statement:
         tay
         ldax    #operator_name_table
         jsr     list_tokenized_name
-        bcc     @add_whitespace_if_not_end       ; Unconditional because list_tokenized_name always clears carry
+        bcc     @add_whitespace_if_not_end      ; Unconditional because list_tokenized_name always clears carry
 
 @not_operator:
         txa                             ; Restore original byte
