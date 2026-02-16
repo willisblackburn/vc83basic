b0030107932c9aaa73a76c3b7328d0fa2bd2ba23
Willis Blackburn <wboyce@panix.com>
2026-02-16 12:01:13 -0500

    Fix VAL bug

--- functions.s
+++ functions.s
@@ -248,8 +248,8 @@ fun_usr:
 
 fun_val:
         jsr     pop_string              ; Get the argument string
+        jsr     load_s0                 ; Into S0, returns length in A
         sta     D                       ; Store the length into D
-        jsr     load_s0                 ; Into S0
         mvax    #buffer, dst_ptr        ; Copy
         ldax    S0
         ldy     D
