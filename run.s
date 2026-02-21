21a27c2e7d6cf098b4bfce67d1e9e23b274567d2
Willis Blackburn <wboyce@panix.com>
2026-02-21 18:43:19 -0500

    Simplify build some more

--- run.s
+++ run.s
@@ -21,25 +21,3 @@ exec_run:
 
 @done:
         rts
-
-; Executes the line pointed by line_ptr
-
-run_line:
-        mva     #.sizeof(Line), line_pos    ; Initialize read position to start of data
-        jsr     decode_byte             ; Get statement number
-        jsr     invoke_statement_handler
-        rts
-
-statement_exec_vectors:
-        .word   exec_run-1
-        .word   exec_print-1
-        .word   exec_let-1
-
-; Invokes a statement handler from a table.
-; This function does not return; it jumps to the handler, which will eventually return.
-; A = the index of the handler in the table
-
-invoke_statement_handler:
-        tay
-        ldax    #statement_exec_vectors
-        jmp     invoke_indexed_vector
