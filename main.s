e36cecee01412af1b980f1a41953843b182f1dde
Willis Blackburn <wboyce@panix.com>
2026-02-21 18:43:19 -0500

    Simplify build some more

--- main.s
+++ main.s
@@ -120,7 +120,40 @@ main:
         jsr     newline
         jmp     @get_command
 
+; Decodes and executes one statement from the token stream.
 
+exec_statement:
+        jsr     decode_byte             ; Get statement number
+        tay
+        ldax    #statement_exec_vectors
+        jmp     invoke_indexed_vector
+
+statement_exec_vectors:
+        .word   exec_end-1
+        .word   exec_run-1
+        .word   exec_print-1
+        .word   exec_let-1
+        .word   exec_input-1
+        .word   exec_list-1
+        .word   exec_goto-1
+        .word   exec_gosub-1
+        .word   exec_return-1
+        .word   exec_pop-1
+        .word   exec_on_goto-1
+        .word   exec_on_gosub-1
+        .word   exec_for-1
+        .word   exec_next-1
+        .word   exec_stop-1
+        .word   exec_cont-1
+        .word   exec_if-1
+        .word   exec_new-1
+        .word   exec_clr-1
+        .word   exec_dim-1
+        .word   exec_rem-1
+        .word   exec_data-1
+        .word   exec_read-1
+        .word   exec_restore-1
+        .word   exec_poke-1
 
 print_start:
         ldax    #start_message
