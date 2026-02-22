0c70f52d647958191af15995205423186af3f43f
Willis Blackburn <wboyce@panix.com>
2026-02-22 16:34:57 -0500

    Handle reset button on Apple II

--- apple2/apple2_startup.s
+++ apple2/apple2_startup.s
@@ -13,13 +13,18 @@ startup:
         jsr     initialize_once     
         jsr     main        
         jmp     DOSWARM                 ; Exit to resident program
-        
+
+reset_handler:
+        raise   PS_READY
+
 .segment "ONCE"     
         
 initialize_once:        
         cld                             ; Clear decimal flag
         lda     #$FF                    ; Print in normal mode
-        sta     COUTMASK        
+        sta     COUTMASK
+        mvax    #reset_handler, SOFTEV  ; RESET button returns control to this program
+        mva     #(>reset_handler ^ $A5), PWREDUP        
         jmp     HOME                    ; Clear screen
 
 .code
