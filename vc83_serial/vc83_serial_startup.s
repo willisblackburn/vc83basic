5103bdfca8662010d91cbdf38cd4666b1dcb3e55
Willis Blackburn <wboyce@panix.com>
2026-05-27 09:42:39 -0400

    Use $D000 for I/O

--- vc83_serial/vc83_serial_startup.s
+++ vc83_serial/vc83_serial_startup.s
@@ -10,13 +10,6 @@ startup:
         cld                             ; Clear decimal flag
         ldx     #$FF        
         txs                             ; Initialize the stack to $FF
-
-        ; Map $Cxxx to $30xxx
-        ; The system uses ZP $02 to map $Cxxx to 20-bit addresses.
-        ; $30100 is in the range mapped by $30.
-        lda     #$30
-        sta     $02
-
         jsr     initialize_target
         jsr     main        
 @halt:
